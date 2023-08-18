07482c60949cdaf3282e2fa08dc86a1851dfd79e
Rob van Hoose <Rob.vanHoose@bankrate.com>
2023-08-18 16:09:04 -0400

    format files

--- tags-merge-assignment.tf
+++ tags-merge-assignment.tf
@@ -8,6 +8,6 @@ resource "aws_s3_bucket" "tags_vars_assignment" {
   name = "test-case"
 
   tags = merge(var.other_tags, {
-    BusinessUnit : "required"
+    BusinessUnit = "required"
   })
 }
--- tags-merge-vars-assignment.tf
+++ tags-merge-vars-assignment.tf
@@ -1,7 +1,8 @@
 locals {
-  basic_tags = { Arbitrary = "optional" }
+  basic_tags    = { Arbitrary = "optional" }
   required_tags = { Team = "optional", BusinessUnit = "required" }
 }
+
 resource "aws_s3_bucket" "tags_vars_assignment" {
   name = "test-case"
 
