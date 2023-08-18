c24bac770a91a780cd070edd1c0ce84491d3d117
Rob van Hoose <Rob.vanHoose@bankrate.com>
2023-08-18 16:07:55 -0400

    add merge tests

---
+++ tags-merge-assignment.tf
@@ -0,0 +1,13 @@
+variable "other_tags" {
+  default = {
+    ArbitraryName = "optional"
+  }
+}
+
+resource "aws_s3_bucket" "tags_vars_assignment" {
+  name = "test-case"
+
+  tags = merge(var.other_tags, {
+    BusinessUnit : "required"
+  })
+}
---
+++ tags-merge-vars-assignment.tf
@@ -0,0 +1,9 @@
+locals {
+  basic_tags = { Arbitrary = "optional" }
+  required_tags = { Team = "optional", BusinessUnit = "required" }
+}
+resource "aws_s3_bucket" "tags_vars_assignment" {
+  name = "test-case"
+
+  tags = merge(local.basic_tags, local.required_tags)
+}
